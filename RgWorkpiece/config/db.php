ea181729064ad824e386a968b7af31c27726ae1a
Kiss Norbert <nn@desktop1.lan>
2019-01-07 12:57:53 +0100

    dev

--- RgWorkpiece/config/db.php
+++ RgWorkpiece/config/db.php
@@ -3,9 +3,10 @@
 return [
     'class' => 'yii\db\Connection',
     //'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'dsn' => 'sqlite:../rgraktarkeszlet.db',
-    //'username' => 'root',
-    //'password' => '',
+    'dsn' => 'mysql:host=norbiiroda.ipdns.hu;dbname=rgworkpiece',
+    //'dsn' => 'sqlite:../rgraktarkeszlet.db',
+    'username' => 'rgworkpiece',
+    'password' => '1hWE4y6JhmvIZBoYpZ3lWptpdKU6EGnD',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
